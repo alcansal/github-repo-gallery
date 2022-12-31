af64605959b50a03503f13343829496b9d062733
alcansal000 <salvador.alcantar98@gmail.com>
2022-12-30 18:45:37 -0600

    Fetching repos and displaying info about repos on the page

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const profileInfo = document.querySelector(".overview");
 // This will be the Git hub username
 const username = "alcansal";
 
+// This will select the unordered list element with the class of repo-list
+const repoList = document.querySelector(".repo-list");
+
 // async function to fetch information for github profile 
 const githubProfile = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -29,5 +32,22 @@ const displayUserInfo = function (data) {
 </div>`;
 
 profileInfo.append(newDiv);
+getRepos();
 };
 
+// This async function will fetch repos
+const getRepos = async function () {
+  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const data = await fetchRepos.json();
+  // console.log(data);
+  displayRepos(data);
+};
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    let repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
+};
